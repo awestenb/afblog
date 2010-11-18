af3b1b9702b00e18fa0901640128b260c40f6da1
Alan Westenbroek <alan@westenbroek.com>
2010-11-18 17:51:35 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afblog::Application.config.secret_token = 'cf1de8b029afc2caa4a1d9c100b7fbc95eddc0285a632416ffbb783a4273b4d1909729e387e46a380f231d450f52d120248e8ece98b20a296bbb1434fe083af2'
